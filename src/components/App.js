62eefc5334ea0e779224d3882e714e74e3b36842
Connor Hansen <chansen13@georgefox.edu>
2021-11-15 09:56:47 -0800

    whiteboard plans.
    Co-authored-by: Roman Kolivashko <rkolivashko@gmail.com>
    Co-authored-by: Brandon Eads <brandon181989@hotmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <Profile />
+      <AboutMe />
+      <Feed />
+
+    </React.Fragment>
+  );
+}
+
+export default App;
